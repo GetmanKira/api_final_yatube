9a191bfb87e46ba6cdb19e2675dbe1886fb23cc2
Kira Getman <kirochkagg@yandex.ru>
2025-04-17 11:04:44 +0300

    fgh

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,18 +4,6 @@ from django.db import models
 User = get_user_model()
 
 
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
-
-    def __str__(self):
-        return self.text
-
-
 class Group(models.Model):
     """Модель групп."""
     title = models.CharField(max_length=200)
@@ -24,6 +12,19 @@ class Group(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts')
+    image = models.ImageField(
+        upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.text
 
 
 class Comment(models.Model):
